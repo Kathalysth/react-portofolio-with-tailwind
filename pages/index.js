1e6718dd96ed4e7d4b3063b64a1c52e16afe7c96
Chris Ehigimetor <bishopsirhchris@gmail.com>
2023-01-03 16:46:46 +0100

    fix: adjust bio

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
               Software Engineer.
             </h3>
             <p className="text-md text-justify py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              I am a Software Engineer with experience in React, NodeJs, and
+              {` I am a Software Engineer with experience in React, NodeJs, and
               Vue. I am also experienced in Java and C#. I am passionate about
               problem-solving, and I am very analytical and results-oriented. I
               am also relentless when it comes to achieving results. I grew up
@@ -110,7 +110,7 @@ export default function Home() {
               that make people's lives better. I am currently working on a
               project that is aimed at making the web more accessible for people
               with disabilities. I also work on contributing to open-source
-              projects frequently.
+              projects frequently.`}
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a
