da9c050e21ca2308d6dd72f62e048f114abeec30
Chris Ehigimetor <bishopsirhchris@gmail.com>
2023-03-03 18:16:21 +0100

    new layout and styling

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import {
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import Hero from "../components/Hero";
 import frameworks from "../public/Code Development _Two Color.svg";
 import versionControl from "../public/Startup_Monochromatic.svg";
 import chatSvg from "../public/Chat_Monochromatic.svg";
@@ -75,7 +76,7 @@ export default function Home() {
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
       />
-      <div className={darkMode ? "dark" : ""}>
+      <div className={`bg-white ${darkMode ? "dark" : ""}`}>
         <Head>
           <title>Chris Ehigimetor</title>
           <meta
@@ -84,154 +85,168 @@ export default function Home() {
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main className=" bg-white px-6 dark:bg-gray-900 md:px-10 lg:px-30">
-          <section className="min-h-screen">
-            <nav className="py-10 mb-12 flex flex-wrap justify-between dark:text-white">
-              <h1 className="font-burtons text-xl">{me}</h1>
-              <ul className="flex items-center">
-                <li className="p-2 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-800">
-                  <BsFillMoonStarsFill
-                    onClick={() => setDarkMode(!darkMode)}
-                    className="  text-2xl"
-                  />
-                </li>
-                <li>
-                  <a
-                    className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                    href="Chris_ehigimetor_cv.pdf"
-                    download
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Download Resume
-                  </a>
-                </li>
-              </ul>
-            </nav>
-            <div className="text-center py-10 flex flex-wrap">
-              <div className="w-full md:w-1/2">
-                <div className="mx-auto flex justify-center relative overflow-hidden">
-                  <Image
-                    src={portrait}
-                    className="rounded-full"
-                    objectFit="cover"
-                    width={400}
-                    height={400}
-                  />
+        <Hero setDark={setDarkMode} dark={darkMode} />
+        <main className="px-6 dark:bg-gray-900 md:px-10 lg:px-30">
+          <section id="about" className="min-h-screen">
+            <div className="py-24 sm:py-32">
+              <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-5xl lg:text-center">
+                  <h2 className="text-base font-semibold leading-7 text-teal-600">
+                    About Me
+                  </h2>
+                  <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-200">
+                    Everything you need to know about me
+                  </p>
+                  <div className="text-center py-10 flex flex-wrap">
+                    <div className="w-full md:w-1/2">
+                      <p
+                        data-aos="fade-up"
+                        data-aos-once="true"
+                        className="text-md mb-4 text-justify py-5 leading-8 text-gray-800 w-full dark:text-gray-200 max-w-xl mx-auto md:text-xl"
+                      >
+                        {` A highly motivated and dedicated software developer with experience in Node.js, React.js, JavaScript, Java, and C#. Possesses strong problem-solving skills and a passion for coding. A quick learner with a strong drive to constantly improve and expand upon existing skills and knowledge. Demonstrates expertise in developing and maintaining high-quality applications, ensuring they meet project requirements and customer expectations. A team player with excellent communication skills, capable of working collaboratively with cross-functional teams to deliver successful projects on time and within budget. Overall, an exceptional software developer who is dedicated to delivering top-notch solutions and continuously improving my craft.`}
+                      </p>
+                    </div>
+                    <div className="w-full md:w-1/2 flex items-start gap-4 pl-8">
+                      <div className="mx-auto md:mx-0 flex justify-center relative overflow-hidden">
+                        <Image
+                          src={portrait}
+                          className="rounded-xl"
+                          objectFit="cover"
+                          width={250}
+                          height={250}
+                        />
+                      </div>
+                      <div className="flex flex-col items-start">
+                        <h3
+                          data-aos="fade-up-left"
+                          data-aos-once="true"
+                          className="text-2xl text-base font-semibold leading-7 tracking-tight dark:text-white text-gray-900"
+                        >
+                          {me}
+                        </h3>
+                        <p
+                          data-aos="fade-up-right"
+                          data-aos-once="true"
+                          className="text-sm font-semibold leading-6 text-teal-600"
+                        >
+                          {role}
+                        </p>
+                        <div className="mt-4 flex justify-center gap-4 p-2 rounded-xl dark:bg-gray-800/[0.8] text-gray-300 bg-slate-100/[0.5]">
+                          <a
+                            className="hover:text-blue-500 dark:hover:bg-white  "
+                            href="https://twitter.com/bishopsirhchris"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <AiFillTwitterCircle size={24} />
+                          </a>
+                          <a
+                            className="hover:text-blue-500 dark:hover:bg-white  "
+                            href="https://www.linkedin.com/in/ambassador-chris-ehigimetor"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <AiFillLinkedin size={24} />
+                          </a>
+                          <a
+                            className="hover:text-black dark:hover:text-white"
+                            href="https://github.com/kathalysth"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <AiFillGithub size={24} />
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <h2
-                  data-aos="fade-up-left"
-                  data-aos-once="true"
-                  className="text-3xl py-2 text-teal-600 font-bold dark:text-teal-400 md:text-4xl"
-                >
-                  {me}
-                </h2>
-                <h3
-                  data-aos="fade-up-right"
-                  data-aos-once="true"
-                  className="text-2xl py-2 dark:text-white md:text-3xl"
-                >
-                  {role}
-                </h3>
-                <div className="text-5xl flex justify-center gap-16 py-3 text-gray-300">
-                  <a
-                    className="hover:text-blue-500  "
-                    href="https://twitter.com/bishopsirhchris"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <AiFillTwitterCircle />
-                  </a>
-                  <a
-                    className="hover:text-blue-500 "
-                    href="https://www.linkedin.com/in/ambassador-chris-ehigimetor"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <AiFillLinkedin />
-                  </a>
-                  <a
-                    className="hover:text-black dark:hover:text-white"
-                    href="https://github.com/kathalysth"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <AiFillGithub />
-                  </a>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2">
-                <p
-                  data-aos="fade-up"
-                  data-aos-once="true"
-                  className="text-md mb-4 text-justify py-5 leading-8 text-gray-800 w-full dark:text-gray-200 max-w-xl mx-auto md:text-xl"
-                >
-                  {` A highly motivated and dedicated software developer with experience in Node.js, React.js, JavaScript, Java, and C#. Possesses strong problem-solving skills and a passion for coding. A quick learner with a strong drive to constantly improve and expand upon existing skills and knowledge. Demonstrates expertise in developing and maintaining high-quality applications, ensuring they meet project requirements and customer expectations. A team player with excellent communication skills, capable of working collaboratively with cross-functional teams to deliver successful projects on time and within budget. Overall, an exceptional software developer who is dedicated to delivering top-notch solutions and continuously improving my craft.`}
-                </p>
-                <a
-                  href="#letsTalk"
-                  className="bg-teal-500 p-4 text-3xl text-white hover:opacity-90"
-                >
-                  Lets Talk
-                </a>
               </div>
             </div>
           </section>
           <section>
-            <div>
-              <h3 className="text-3xl py-1 dark:text-white font-bold ">
-                Skills
-              </h3>
-            </div>
-            <div className="lg:flex gap-10">
-              <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-gray-700 flex-1">
-                <Image
-                  src={programming}
-                  width={200}
-                  height={200}
-                  alt="languages figure"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600 ">
-                  Languages
-                </h3>
-                <p className="text-gray-900 dark:text-gray-200 py-1">
-                  Javascript
-                </p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">C#</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Java</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Python</p>
-              </div>
-              <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 flex-1">
-                <Image
-                  src={frameworks}
-                  width={200}
-                  height={200}
-                  alt="technologies figure"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600 ">
-                  Technologies and Frameworks
-                </h3>
-                <p className="text-gray-900 dark:text-gray-200 py-1">React</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">
-                  Spring Boot
-                </p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">.Net</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Vue</p>
-              </div>
-              <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 flex-1">
-                <Image
-                  src={versionControl}
-                  width={200}
-                  height={200}
-                  alt="verson control figure"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600">
-                  CI/CD
-                </h3>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Linux</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Scrum</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Git</p>
-                <p className="text-gray-900 dark:text-gray-200 py-1">Docker</p>
+            <div className="py-24 sm:py-32">
+              <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-5xl lg:text-center">
+                  <h2 className="text-base font-semibold leading-7 text-teal-600">
+                    Skills
+                  </h2>
+                  <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                    {/* Everything you need to know about me */}
+                  </p>
+                  <div className="lg:flex gap-10">
+                    <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-gray-700 flex-1">
+                      <Image
+                        src={programming}
+                        width={200}
+                        height={200}
+                        alt="languages figure"
+                      />
+                      <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600 ">
+                        Languages
+                      </h3>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Javascript
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        C#
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Java
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Python
+                      </p>
+                    </div>
+                    <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 flex-1">
+                      <Image
+                        src={frameworks}
+                        width={200}
+                        height={200}
+                        alt="technologies figure"
+                      />
+                      <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600 ">
+                        Technologies and Frameworks
+                      </h3>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        React
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Spring Boot
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        .Net
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Vue
+                      </p>
+                    </div>
+                    <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 flex-1">
+                      <Image
+                        src={versionControl}
+                        width={200}
+                        height={200}
+                        alt="verson control figure"
+                      />
+                      <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600">
+                        CI/CD
+                      </h3>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Linux
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Scrum
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Git
+                      </p>
+                      <p className="text-gray-900 dark:text-gray-200 py-1">
+                        Docker
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
